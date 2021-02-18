4b2fd24a80aef5900c4ace442417427687822c96
rcarney34 <47376375+rcarney34@users.noreply.github.com>
2021-02-18 02:09:31 -0800

    Update cs-sketch.js

--- cs-sketch.js
+++ cs-sketch.js
@@ -1,5 +1,5 @@
-// Authors: Albert Paez, Ryan Patrick
-// Description: P5 key animation fcns.  
+// Authors: Albert Paez, Ryan Patrick, Ryan Carney
+// Description: P5 key animation fcns.
 // Add. Info: p5js.org/reference
 
 // Make global g_canvas JS 'object': a key-value 'dictionary'.
@@ -25,6 +25,7 @@ var colorsArray = ['blue', 'yellow', 'red', 'black']; //Array for colors
 var mode = 0; // { 0 = LR Mode, 1 = Set-Count Mode, 2 = Countdown Mode }
 var straightCounter = 0;
 var moveCounter = 1;
+var dir = 0;
 
 function setup() // P5 Setup Fcn
 {
@@ -32,7 +33,7 @@ function setup() // P5 Setup Fcn
     let width = sz * g_canvas.wid;  // Our 'canvas' uses cells of given size, not 1x1 pixels.
     let height = sz * g_canvas.hgt;
     createCanvas( width, height );  // Make a P5 canvas.
-    draw_grid( 10, 50, 'gray', 'white' );
+    draw_grid( 10, 50, 'gray', 'white ' );
     console.log("#" + moveCounter + " {p=(" + g_bot.x + "," + g_bot.y + "), d=" + g_bot.dir + ", m= " + mode + ", i=" + straightCounter
     + "}; {c=" + colorsArray[cellStates[g_bot.x][g_bot.y]] + "}");
     moveCounter++;
@@ -75,8 +76,9 @@ function turnLeft(){
 
 function setCount(){
     mode = 1;
-    let direction = g_bot.dir;
-    return direction;
+    //let direction = g_bot.dir;
+    //g_bot.dir = direction;
+    return g_bot.dir;
 }
 
 
@@ -84,12 +86,12 @@ function setCount(){
 
 function move_bot()
 {
-    let dir = 0;
-    let dx = 0; 
-    let dy = 0; 
+    //let dir = 0;
+    let dx = 0;
+    let dy = 0;
 
     // We are in LR Mode, choose direction based on cell color
-    if (mode === 0){  
+    if (mode === 0){
         if (cellStates[g_bot.x][g_bot.y] === 3){
             dir = turnLeft();
         }
@@ -120,9 +122,9 @@ function move_bot()
     }
     else { // We are in Countdown Mode, continue straight
         straightCounter--;
-        if(straightCounter <= 0) {
+       if(straightCounter < 0) {
             mode = 0;
-        }
+       }
     }
 
     switch (dir) { // Convert dir to x,y deltas: dir = clock w 0=Up,1=Rt,2=Dn,3=Left.
@@ -133,7 +135,7 @@ function move_bot()
     }
 
     cellStates[g_bot.x][g_bot.y] = (cellStates[g_bot.x][g_bot.y] + 1) % colorsArray.length;
-      
+
     let x = (dx + g_bot.x + g_box.wid) % g_box.wid; // Move-x.  Ensure positive b4 mod.
     let y = (dy + g_bot.y + g_box.hgt) % g_box.hgt; // Ditto y.
 
